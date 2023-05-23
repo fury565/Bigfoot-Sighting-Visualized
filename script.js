b22f830754018ad91c23081a6d2ba3ced78ac512
bruno_licht <legotoster@gmail.com>
2023-05-23 09:39:19 +0200

    Added chart data selection

--- script.js
+++ script.js
@@ -2,13 +2,25 @@ const circleContainer=document.querySelector('.circle-container');
 const detailsContainer=document.querySelector('.details-tab');
 const yearSelector=document.querySelector('#years');
 const yearCounter=document.querySelector('.year-counter');
+const chartSelector=document.querySelector('#chartSelector');
+const chartXSize = 800;
+const chartYSize = 500;
+const chartMargin = 40;
+const chartXMax = chartXSize - chartMargin*2;
+const chartYMax = chartYSize - chartMargin*2;
+const svg = d3.select("#myPlot").append("svg")
+  .append("g")
+  .attr("transform","translate(" + chartMargin + "," + chartMargin + ")")
+  .attr("class","chartHolder")
+  ;
 var data =[];
-
+var chartData=[];
 async function getData(){
     await fetch('./converted.json')
     .then((response) => response.json())
     .then((json) => data=json)
     fillMap();
+    afterFetchInit();
 }
 
 function fillMap(){
@@ -24,10 +36,6 @@ function addToMap(reportJson){
     newCircle.style.position='absolute';
     newCircle.style.top=(49-reportJson.latitude)/24*631;
     newCircle.style.left=(reportJson.longitude+125)/58*1167;
-    if(newCircle.style.top<-10){
-        console.log(2);
-    }
-        
     newCircle.innerHTML=`
         <circle cx="3" cy="3" r="3" fill="red" />
     `;
@@ -36,9 +44,9 @@ function addToMap(reportJson){
 }
 function setDetails(reportJson){
     detailsContainer.innerHTML=`
-        <p>${reportJson.title}</p> 
-        <p>${reportJson.latitude}</p>
-        <p>${reportJson.longitude}</p> 
+        <p>Report Details:</p>
+        <p>Title: ${reportJson.title}</p> 
+        <p>Date: ${reportJson.date}</p> 
     `;
 }
 function reloadMap(year){
@@ -61,6 +69,175 @@ function populateOptions(min,max){
         yearSelector.appendChild(option);
     }
 }
+
+
+function setChartData(chartType){
+    if(chartType==="1"){
+      let seasonData=data.map((element) => element.season);
+      let seasonCounts={};
+      seasonData.forEach((season) => {
+        if (seasonCounts[season]) {
+          seasonCounts[season]++;
+        } 
+        else {
+          seasonCounts[season] = 1;
+        }
+      });
+      chartData=Object.entries(seasonCounts);
+      toSeasonChart();
+    }
+    else if(chartType==="2"){
+      let classificationData=data.map((element) => element.classification);
+      let classificationCounts={};
+      classificationData.forEach((classification) => {
+        if (classificationCounts[classification]) {
+          classificationCounts[classification]++;
+        } 
+        else {
+          classificationCounts[classification] = 1;
+        }
+      });
+      chartData=Object.entries(classificationCounts);
+      toClassificationChart();
+    }
+    else if(chartType==="3"){
+      let weatherData=data.map((element) => element.summary);
+      let weatherCounts={};
+      weatherData.forEach((weather) => {
+        if (weatherCounts[weather]) {
+          weatherCounts[weather]++;
+        } 
+        else {
+          weatherCounts[weather] = 1;
+        }
+      });
+      chartData=Object.entries(weatherCounts);
+      toWeatherChart();
+    }
+}
+
+
+function toSeasonChart(){
+  d3.selectAll(".chartHolder > *").remove();
+    
+  var x = d3.scaleBand().range([0, chartXMax]).padding(0.1)
+      .domain(chartData.map((d)=>d[0]));
+    
+  svg.append("g")
+    .attr("transform", "translate(0," + chartYMax + ")")
+    .call(d3.axisBottom(x));
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(chartData, (d) => d[1])])
+    .range([ chartYMax, 0]);
+    
+  svg.append("g")
+    .call(d3.axisLeft(y));
+    
+  svg
+    .selectAll(".bar")
+    .data(chartData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => x(d[0]))
+    .attr("y", (d) => y(d[1]))
+    .attr("width", 100)
+    .attr("height", (d) => chartYMax - y(d[1]));
+        
+  svg.append("text")
+    .attr("x", chartXMax/2)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px").text("Number of reports per season");
+}
+
+
+function toClassificationChart(){
+  d3.selectAll(".chartHolder > *").remove();
+  svg.append("svg")
+    .append("g")
+    .attr("transform","translate(" + chartMargin + "," + chartMargin + ")")
+  ;
+    
+  var x = d3.scaleBand().range([0, chartXMax]).padding(0.1)
+      .domain(chartData.map((d)=>d[0]));
+    
+  svg.append("g")
+    .attr("transform", "translate(0," + chartYMax + ")")
+    .call(d3.axisBottom(x));
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(chartData, (d) => d[1])])
+    .range([ chartYMax, 0]);
+    
+  svg.append("g")
+    .call(d3.axisLeft(y));
+    
+  svg
+    .selectAll(".bar")
+    .data(chartData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => x(d[0]))
+    .attr("y", (d) => y(d[1]))
+    .attr("width", 100)
+    .attr("height", (d) => chartYMax - y(d[1]));
+        
+  svg.append("text")
+    .attr("x", chartXMax/2)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px").text("Types of reports: A-visual B-noises C-other");
+}
+
+
+function toWeatherChart(){
+  d3.selectAll(".chartHolder > *").remove();
+  svg.append("svg")
+    .append("g")
+    .attr("transform","translate(" + chartMargin + "," + chartMargin + ")")
+  ;
+    
+  var x = d3.scaleBand().range([0, chartXMax]).padding(0.1)
+      .domain(chartData.map((d)=>d[0]));
+    
+  svg.append("g")
+    .attr("transform", "translate(0," + chartYMax + ")")
+    .call(d3.axisBottom(x));
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(chartData, (d) => d[1])])
+    .range([ chartYMax, 0]);
+    
+  svg.append("g")
+    .call(d3.axisLeft(y));
+    
+  svg
+    .selectAll(".bar")
+    .data(chartData)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", (d) => x(d[0]))
+    .attr("y", (d) => y(d[1]))
+    .attr("width", 100)
+    .attr("height", (d) => chartYMax - y(d[1]));
+        
+  svg.append("text")
+    .attr("x", chartXMax/2)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .style("font-size", "16px").text("Weather during report");
+}
+
+
+function afterFetchInit(){
+  setChartData("1");
+}
+
 getData();
 populateOptions(1953,2021);
 yearSelector.addEventListener('change',()=>reloadMap(yearSelector.value));
+chartSelector.addEventListener('change',()=>setChartData(chartSelector.value));
